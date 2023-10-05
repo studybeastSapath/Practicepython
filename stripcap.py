d04db6ac49987b9b83155681a04f9d21a947c981
Sapath Chakraborty <sapathchakraborty2@gmail.com>
2023-10-05 12:40:37 +0530

    perform strip and cap

---
+++ stripcap.py
@@ -0,0 +1,8 @@
+#Ask user their name
+name= input("What's the name of the user ?")
+
+#Remove all whitespaces and capitalise the first letter of the name and title of the user input
+name= name.strip().capitalize()
+
+#Greet the user
+print(f"Hello, {name}")
